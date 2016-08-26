64d473828a8991b57ee940d165d2f5daf314420f
Paren Desai <paren@Parens-MacBook-Pro.local>
2016-08-26 15:43:19 +0530

    Readded SQL File

---
+++ db.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 26, 2016 at 11:29 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `placements`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog`
+--
+
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL,
+  `slug` varchar(100) NOT NULL,
+  `blog` longblob NOT NULL,
+  `cid` int(11) NOT NULL,
+  `sapid` varchar(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `approved` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companydetails`
+--
+
+CREATE TABLE `companydetails` (
+  `id` int(11) NOT NULL,
+  `companyid` int(11) NOT NULL,
+  `userdetail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companyuser`
+--
+
+CREATE TABLE `companyuser` (
+  `companyid` int(11) NOT NULL,
+  `sapid` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `slug` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `descr` longblob NOT NULL,
+  `open` varchar(10) NOT NULL DEFAULT 'open',
+  `deleted` int(11) DEFAULT '0',
+  `mincgpa` float NOT NULL DEFAULT '0',
+  `minssc` float NOT NULL DEFAULT '0',
+  `minhsc` float NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queries`
+--
+
+CREATE TABLE `queries` (
+  `id` int(11) NOT NULL,
+  `answered` int(11) NOT NULL DEFAULT '0',
+  `cid` int(11) NOT NULL,
+  `sapid` varchar(20) NOT NULL,
+  `querytext` longblob NOT NULL,
+  `answer` longblob
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `updates`
+--
+
+CREATE TABLE `updates` (
+  `id` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `date` varchar(30) NOT NULL,
+  `updts` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `sap` varchar(11) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `fname` varchar(100) DEFAULT NULL,
+  `lname` varchar(100) DEFAULT NULL,
+  `ssc` float DEFAULT NULL,
+  `sscYear` varchar(10) DEFAULT NULL,
+  `hsc` float DEFAULT NULL,
+  `hscYear` varchar(10) NOT NULL,
+  `cgpa` float DEFAULT NULL,
+  `beper` double NOT NULL DEFAULT '0',
+  `curBacklog` int(11) NOT NULL DEFAULT '0',
+  `pastBacklog` int(11) NOT NULL DEFAULT '0',
+  `address` longblob,
+  `internships` longblob,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `dob` varchar(100) NOT NULL,
+  `preflang` varchar(100) DEFAULT NULL,
+  `updated` int(1) NOT NULL DEFAULT '0',
+  `admin` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `companydetails`
+--
+ALTER TABLE `companydetails`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `full` (`companyid`,`userdetail`);
+
+--
+-- Indexes for table `companyuser`
+--
+ALTER TABLE `companyuser`
+  ADD UNIQUE KEY `companyid` (`companyid`,`sapid`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `queries`
+--
+ALTER TABLE `queries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `updates`
+--
+ALTER TABLE `updates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sap` (`sap`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `companydetails`
+--
+ALTER TABLE `companydetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `queries`
+--
+ALTER TABLE `queries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `updates`
+--
+ALTER TABLE `updates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=152;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
